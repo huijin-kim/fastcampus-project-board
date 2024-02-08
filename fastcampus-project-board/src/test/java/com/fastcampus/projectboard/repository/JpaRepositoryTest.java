01152d78d771a18212df2ec0b847cfe31d9a0128
HUIJIN KIM <hijno@naver.com>
2024-02-08 22:43:21 +0900

    #10DB: Auto stash before rebase of "refs/heads/feature/#10DB"

--- fastcampus-project-board/src/test/java/com/fastcampus/projectboard/repository/JpaRepositoryTest.java
+++ fastcampus-project-board/src/test/java/com/fastcampus/projectboard/repository/JpaRepositoryTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 
 //import static org.junit.jupiter.api.Assertions.*;
 
@@ -15,6 +16,7 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
+@ActiveProfiles("testdb")
 @DisplayName("JPA connect test")
 @Import(JpaConfig.class)
 @DataJpaTest
@@ -43,5 +45,52 @@ class JpaRepositoryTest {
 
     }
 
-    
+    @DisplayName("insert 테스트")
+    @Test
+    void givenTestData_whenInserting_thenWorksFine() {
+        //given
+        long previousCount = articleRepository.count();
+
+        //when
+        Article savedArticle = articleRepository.save(Article.of("new article", "new content", "#spring"));
+
+        //work
+        assertThat(articleRepository.count()).isEqualTo(previousCount + 1);
+    }
+
+    @DisplayName("update 테스트")
+    @Test
+    void givenTestData_whenUpdating_thenWorksFine() {
+        //given
+        Article article = articleRepository.findById(1L).orElseThrow();
+        String updatedHashtag = "#springboot";
+        article.setHashtag(updatedHashtag);
+        long previousCount = articleRepository.count();
+
+        //when
+//        Article savedArticle = articleRepository.save(article);   //예전에는 동일하게 존재하면 rollback하기에 update 쿼리가 안등장한다고한다. 하지만 나는 등장 했다.
+        Article savedArticle = articleRepository.saveAndFlush(article);   //현재 버전에서 이가 차이점이 있나?
+
+        //work
+        assertThat(savedArticle).hasFieldOrPropertyWithValue("hashtag", updatedHashtag);
+    }
+
+    @DisplayName("insert 테스트")
+    @Test
+    void givenTestData_whenDeleting_thenWorksFine() {
+        //given
+        Article article = articleRepository.findById(1L).orElseThrow();
+        long previousArticleCount = articleRepository.count();
+        long previousArticleCommentCount = articleCommentRepository.count();
+        long deletedCommentSize = article.getArticleComment().size();
+
+        //when
+        articleRepository.delete(article);
+
+        //work
+        assertThat(articleRepository.count()).isEqualTo(previousArticleCount - 1);
+        assertThat(articleCommentRepository.count()).isEqualTo(previousArticleCommentCount - deletedCommentSize);
+    }
+
+
 }
